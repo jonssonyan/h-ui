70430700b00ed7ba13b49e9a699433128b26637f
jonssonyan <yz808@outlook.com>
2024-04-29 00:45:07 +0800

    fix: h-ui

--- model/dto/account.go
+++ model/dto/account.go
@@ -12,12 +12,12 @@ type LoginDto struct {
 }
 
 type AccountSaveDto struct {
-	Username   *string `json:"username" form:"username" validate:"omitempty,min=6,max=128,validateStr"`
-	Pass       *string `json:"pass" form:"pass" validate:"omitempty,min=6,max=128,validateStr"`
-	ConPass    *string `json:"conPass" form:"conPass" validate:"omitempty,min=6,max=128,validateStr"`
-	Quota      *int64  `json:"quota" form:"quota" validate:"omitempty,min=-1"`
-	ExpireTime *int64  `json:"expireTime" form:"expireTime" validate:"omitempty,min=0"`
-	Deleted    *int64  `json:"deleted" form:"deleted" validate:"omitempty,oneof=0 1"`
+	Username   *string `json:"username" form:"username" validate:"required,min=6,max=128,validateStr"`
+	Pass       *string `json:"pass" form:"pass" validate:"required,min=6,max=128,validateStr"`
+	ConPass    *string `json:"conPass" form:"conPass" validate:"required,min=6,max=128,validateStr"`
+	Quota      *int64  `json:"quota" form:"quota" validate:"required,min=-1"`
+	ExpireTime *int64  `json:"expireTime" form:"expireTime" validate:"required,min=0"`
+	Deleted    *int64  `json:"deleted" form:"deleted" validate:"required,oneof=0 1"`
 }
 
 type AccountUpdateDto struct {
