9786807936d15d00dca7e9f8851ec90dfac0669b
jonssonyan <yz808@outlook.com>
2024-06-06 10:31:17 +0800

    fix: h-ui

--- docs/sql/h_ui_db.sql
+++ docs/sql/h_ui_db.sql
@@ -13,27 +13,20 @@ create table account
     device_no      INTEGER   default 3      not null,
     `role`         TEXT      default 'user' not null,
     deleted        INTEGER   default 0      not null,
-    create_time    TIMESTAMP default CURRENT_TIMESTAMP,
-    update_time    TIMESTAMP default CURRENT_TIMESTAMP
+    create_time    TIMESTAMP default (datetime(CURRENT_TIMESTAMP, 'localtime')),
+    update_time    TIMESTAMP default (datetime(CURRENT_TIMESTAMP, 'localtime'))
 );
-
 create index account_deleted_index
     on account (deleted);
-
 create index account_con_pass_index
     on account (con_pass);
-
 create index account_pass_index
     on account (`pass`);
-
 create index account_username_index
     on account (username);
-
 INSERT INTO account (username, `pass`, con_pass, quota, download, upload, expire_time, device_no, role)
 VALUES ('sysadmin', 'f8cdb04495ded47615258f9dc6a3f4707fd2405434fefc3cbf4ef4e6',
         'c7591c31adf8af0b6b8ae8cbbccd8d1aaa0c7bb068f576bddb6378d5', -1, 0, 0, 253370736000000, 6, 'admin');
-
-
 create table config
 (
     id          INTEGER              not null
@@ -41,13 +34,11 @@ create table config
     `key`       TEXT      default '' not null,
     `value`     TEXT      default '' not null,
     remark      TEXT      default '' not null,
-    create_time TIMESTAMP default CURRENT_TIMESTAMP,
-    update_time TIMESTAMP default CURRENT_TIMESTAMP
+    create_time TIMESTAMP default (datetime(CURRENT_TIMESTAMP, 'localtime')),
+    update_time TIMESTAMP default (datetime(CURRENT_TIMESTAMP, 'localtime'))
 );
-
 create index account_key_index
     on config (`key`);
-
 INSERT INTO config (key, value, remark)
 VALUES ('H_UI_WEB_PORT', '8081', 'H UI Web 端口');
 INSERT INTO config (key, value, remark)
