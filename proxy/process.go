1b3dd725e222f814fd5414df177ba181b8aa7c14
jonssonyan <yz808@outlook.com>
2024-06-07 14:14:16 +0800

    fix: h-ui

--- proxy/process.go
+++ proxy/process.go
@@ -1,9 +1,12 @@
 package proxy
 
 import (
+	"bufio"
 	"context"
 	"errors"
 	"github.com/sirupsen/logrus"
+	"io"
+	"log"
 	"os/exec"
 	"sync"
 	"time"
@@ -19,70 +22,153 @@ func (p *process) IsRunning() bool {
 }
 
 func (p *process) Start(name string, arg ...string) error {
+	if !p.mutex.TryLock() {
+		logrus.Errorf("start cmd err: lock not acquired")
+		return errors.New("start cmd err")
+	}
 	defer p.mutex.Unlock()
-	if p.mutex.TryLock() {
-		if p.IsRunning() {
-			return nil
-		}
 
-		cmd := exec.Command(name, arg...)
-		if cmd.Err != nil {
-			logrus.Errorf("cmd err: %v", cmd.Err)
-			return errors.New("cmd err")
-		}
-		if err := cmd.Start(); err != nil {
-			logrus.Errorf("cmd start err: %v", err)
-			return errors.New("cmd start err")
-		}
-
-		p.cmd = cmd
-
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-		done := make(chan error)
-		go func() {
-			done <- cmd.Wait()
-			select {
-			case err := <-done:
-				if err != nil {
-					logrus.Errorf("cmd wait err: %v", err)
-					_ = p.releaseProcess()
-				}
-			case <-ctx.Done():
-				logrus.Errorf("cmd wait timeout")
-				_ = p.releaseProcess()
-			}
-		}()
+	if p.IsRunning() {
 		return nil
 	}
-	logrus.Errorf("start cmd err: lock not acquired")
-	return errors.New("start cmd err")
+
+	cmd := exec.Command(name, arg...)
+	if cmd.Err != nil {
+		logrus.Errorf("cmd err: %v", cmd.Err)
+		return errors.New("cmd err")
+	}
+
+	// 获取命令的 stdout 和 stderr
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		logrus.Errorf("Error obtaining stdout: %v", err)
+		return err
+	}
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		logrus.Errorf("Error obtaining stderr: %v", err)
+		return err
+	}
+
+	if err := cmd.Start(); err != nil {
+		logrus.Errorf("cmd start err: %v", err)
+		return errors.New("cmd start err")
+	}
+
+	p.cmd = cmd
+
+	go p.handleCmdExecution(cmd)
+
+	go p.handleLogs(stdout, stderr)
+
+	return nil
 }
 
 func (p *process) Stop() error {
+	if !p.mutex.TryLock() {
+		logrus.Errorf("cmd stop err: lock not acquired")
+		return errors.New("cmd stop err")
+	}
 	defer p.mutex.Unlock()
-	if p.mutex.TryLock() {
-		if !p.IsRunning() {
-			return nil
-		}
-		if err := p.cmd.Process.Kill(); err != nil {
-			logrus.Errorf("cmd stop err: %v", err)
-			return errors.New("cmd stop err")
-		}
-		p.cmd = nil
+
+	if !p.IsRunning() {
 		return nil
 	}
-	logrus.Errorf("cmd stop err: lock not acquired")
-	return errors.New("cmd stop err")
+
+	if err := p.cmd.Process.Kill(); err != nil {
+		logrus.Errorf("cmd stop err: %v", err)
+		return errors.New("cmd stop err")
+	}
+	p.cmd = nil
+	return nil
 }
 
-func (p *process) releaseProcess() error {
-	if p.cmd != nil && p.cmd.Process != nil {
-		if err := p.cmd.Process.Release(); err != nil {
-			logrus.Errorf("cmd release err: %v", err)
-			return errors.New("cmd release err")
-		}
-		p.cmd = nil
+func (p *process) release() error {
+	if !p.mutex.TryLock() {
+		logrus.Errorf("cmd release err: lock not acquired")
+		return errors.New("cmd release err")
 	}
+	defer p.mutex.Unlock()
+
+	if !p.IsRunning() {
+		return nil
+	}
+
+	if err := p.cmd.Process.Release(); err != nil {
+		logrus.Errorf("cmd release err: %v", err)
+		return errors.New("cmd release err")
+	}
+	p.cmd = nil
 	return nil
+}
+
+func (p *process) handleCmdExecution(cmd *exec.Cmd) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	done := make(chan error, 1)
+	go func() {
+		done <- cmd.Wait()
+	}()
+
+	select {
+	case err := <-done:
+		if err != nil {
+			logrus.Errorf("cmd wait err: %v", err)
+			_ = p.release()
+		}
+	case <-ctx.Done():
+		logrus.Errorf("cmd wait timeout")
+		_ = p.release()
+	}
+}
+
+func (p *process) handleLogs(stdout, stderr io.ReadCloser) {
+	// 日志
+	stdoutChan := make(chan string)
+	stderrChan := make(chan string)
+
+	go func() {
+		scanner := bufio.NewScanner(stdout)
+		for scanner.Scan() {
+			stdoutChan <- scanner.Text()
+		}
+		if err := scanner.Err(); err != nil {
+			logrus.Errorf("Error reading stdout: %v", err)
+		}
+		close(stdoutChan)
+	}()
+
+	go func() {
+		scanner := bufio.NewScanner(stderr)
+		for scanner.Scan() {
+			stderrChan <- scanner.Text()
+		}
+		if err := scanner.Err(); err != nil {
+			logrus.Errorf("Error reading stderr: %v", err)
+		}
+		close(stderrChan)
+	}()
+
+	for {
+		select {
+		case line, ok := <-stdoutChan:
+			if !ok {
+				stdoutChan = nil
+			} else {
+				log.Println(line)
+			}
+		case line, ok := <-stderrChan:
+			if !ok {
+				stderrChan = nil
+			} else {
+				log.Println(line)
+			}
+		}
+
+		// 当两个 channel 都关闭时，退出循环
+		if stdoutChan == nil && stderrChan == nil {
+			break
+		}
+	}
 }
