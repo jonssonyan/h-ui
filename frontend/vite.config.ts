da582da12bd93cd9ac0fbbae2c4f7fa9ac3f1f21
jonssonyan <yz808@outlook.com>
2024-06-20 10:13:45 +0800

    fix: build file path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,6 +17,10 @@ import path from "path";
 
 const pathSrc = path.resolve(__dirname, "src");
 
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\u0000-\u001F"#$&*+,:;<=>?[\]^`{|}\u007F]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
+
 export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
   const env = loadEnv(mode, process.cwd());
   return {
@@ -98,5 +102,23 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
         symbolId: "icon-[dir]-[name]",
       }),
     ],
+    build: {
+      rollupOptions: {
+        output: {
+          sanitizeFileName(name: string): string {
+            // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+            const match = DRIVE_LETTER_REGEX.exec(name);
+            const driveLetter = match ? match[0] : "";
+
+            // A `:` is only allowed as part of a windows drive letter (ex: C:\foo)
+            // Otherwise, avoid them because they can refer to NTFS alternate data streams.
+            return (
+              driveLetter +
+              name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "_")
+            );
+          },
+        },
+      },
+    },
   };
 });
