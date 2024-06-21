3ce109b75317a5553aa22f0d8aa172188b33f518
jonssonyan <yz808@outlook.com>
2024-06-21 15:45:23 +0800

    fix: sql

--- docs/sql/h_ui_db.sql
+++ docs/sql/h_ui_db.sql
@@ -1,46 +1,37 @@
-create table account
+CREATE TABLE account
 (
-    id             INTEGER                  not null
-        primary key autoincrement,
-    username       TEXT      default ''     not null
-        unique,
-    `pass`         TEXT      default ''     not null,
-    con_pass       TEXT      default ''     not null,
-    quota          INTEGER   default 0      not null,
-    download       INTEGER   default 0      not null,
-    upload         INTEGER   default 0      not null,
-    expire_time    INTEGER   default 0      not null,
-    kick_util_time INTEGER   default 0      not null,
-    device_no      INTEGER   default 3      not null,
-    `role`         TEXT      default 'user' not null,
-    deleted        INTEGER   default 0      not null,
-    create_time    TIMESTAMP default (datetime(CURRENT_TIMESTAMP, 'localtime')),
-    update_time    TIMESTAMP default (datetime(CURRENT_TIMESTAMP, 'localtime'))
+    id             INTEGER PRIMARY KEY AUTOINCREMENT,
+    username       TEXT    NOT NULL UNIQUE DEFAULT '',
+    pass           TEXT    NOT NULL        DEFAULT '',
+    con_pass       TEXT    NOT NULL        DEFAULT '',
+    quota          INTEGER NOT NULL        DEFAULT 0,
+    download       INTEGER NOT NULL        DEFAULT 0,
+    upload         INTEGER NOT NULL        DEFAULT 0,
+    expire_time    INTEGER NOT NULL        DEFAULT 0,
+    kick_util_time INTEGER NOT NULL        DEFAULT 0,
+    device_no      INTEGER NOT NULL        DEFAULT 3,
+    role           TEXT    NOT NULL        DEFAULT 'user',
+    deleted        INTEGER NOT NULL        DEFAULT 0,
+    create_time    TIMESTAMP               DEFAULT CURRENT_TIMESTAMP,
+    update_time    TIMESTAMP               DEFAULT CURRENT_TIMESTAMP
 );
-create index account_deleted_index
-    on account (deleted);
-create index account_con_pass_index
-    on account (con_pass);
-create index account_pass_index
-    on account (`pass`);
-create index account_username_index
-    on account (username);
-INSERT INTO account (username, `pass`, con_pass, quota, download, upload, expire_time, device_no, role)
+CREATE INDEX account_deleted_index ON account (deleted);
+CREATE INDEX account_username_index ON account (username);
+CREATE INDEX account_con_pass_index ON account (con_pass);
+CREATE INDEX account_pass_index ON account (pass);
+INSERT INTO account (username, pass, con_pass, quota, download, upload, expire_time, device_no, role)
 VALUES ('sysadmin', '02f382b76ca1ab7aa06ab03345c7712fd5b971fb0c0f2aef98bac9cd',
         'ee85cbdbb387a61de3d1d52e6773cc31e28c7913b32f4b7aa44ec61b', -1, 0, 0, 253370736000000, 6, 'admin');
-create table config
+CREATE TABLE config
 (
-    id          INTEGER              not null
-        primary key autoincrement,
-    `key`       TEXT      default '' not null
-        unique,
-    `value`     TEXT      default '' not null,
-    remark      TEXT      default '' not null,
-    create_time TIMESTAMP default (datetime(CURRENT_TIMESTAMP, 'localtime')),
-    update_time TIMESTAMP default (datetime(CURRENT_TIMESTAMP, 'localtime'))
+    id          INTEGER PRIMARY KEY AUTOINCREMENT,
+    key         TEXT NOT NULL UNIQUE DEFAULT '',
+    value       TEXT NOT NULL        DEFAULT '',
+    remark      TEXT NOT NULL        DEFAULT '',
+    create_time TIMESTAMP            DEFAULT CURRENT_TIMESTAMP,
+    update_time TIMESTAMP            DEFAULT CURRENT_TIMESTAMP
 );
-create index account_key_index
-    on config (`key`);
+CREATE INDEX config_key_index ON config (key);
 INSERT INTO config (key, value, remark)
 VALUES ('H_UI_WEB_PORT', '8081', 'H UI Web 端口');
 INSERT INTO config (key, value, remark)
