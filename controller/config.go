ad9ac6bb43593bff075473fdbf7de11266a6fdc8
jonssonyan <yz808@outlook.com>
2024-07-12 10:35:43 +0800

    fix: bug

--- controller/config.go
+++ controller/config.go
@@ -69,7 +69,7 @@ func UpdateConfigs(c *gin.Context) {
 
 		if key == constant.Hysteria2ConfigPortHopping {
 			re := regexp.MustCompile("^\\d+(?:-\\d+)?(?:,\\d+(?:-\\d+)?)*$\n")
-			if !re.MatchString(value) {
+			if value != "" && !re.MatchString(value) {
 				vo.Fail(fmt.Sprintf("port hopping: %s is invalid", value), c)
 				return
 			}
